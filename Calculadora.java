c648253e7b0a313017186c6f3bda21e81ba08c2c
Heidi Zela <HeidiZela@ejemplo.com>
2026-08-28 21:22:52 -0500

    Agrega operacion de multiplicacion

--- Calculadora.java
+++ Calculadora.java
@@ -1,10 +1,14 @@
 public class Calculadora {
     public static void main(String[] args) {
         System.out.println("=== Calculadora ===");
-        System.out.println("Operaciones disponibles: suma");
+        System.out.println("Operaciones disponibles: suma, multiplicacion");
     }
 
     public static int sumar(int a, int b) {
         return a + b;
+    }
+
+    public static int multiplicar(int a, int b) {
+        return a * b;
     }
 }
